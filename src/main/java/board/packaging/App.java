1e24f571718548db865b1e230fb8abd2ce4c53d1
Nguyễn Tiến Đạt <112878795+NguyenTienDat377@users.noreply.github.com>
2025-02-13 14:28:14 +0700

    First version

--- src/main/java/board/packaging/App.java
+++ src/main/java/board/packaging/App.java
@@ -17,6 +17,83 @@ import java.util.Map;
  *
  */
 public class App {
+
+    public static class Rectangle {
+        int h;
+        int w;
+        public int c;
+        Rectangle(int h, int w, int c) {
+            this.h = h;
+            this.w = w;
+            this.c = c;
+        }
+    }
+
+    public static MPObjective addObjective(MPSolver solver,
+    int m, int n, int r,
+    int[][] board,
+    Rectangle[] rectangles,
+    MPVariable[][][] x_ijr,
+    MPVariable[][] y_ij,
+    Map<Integer, ArrayList<int[]>> A_r,
+    Map<Integer, Map<int[], ArrayList<int[]>>> B_ijr) {
+        MPObjective objective = solver.objective();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                objective.setCoefficient(y_ij[i][j], board[i][j]);
+            }
+        }
+        for (int k = 0; k < r; k++) {
+            for (int[] pos : A_r.get(k)) {
+                objective.setCoefficient(x_ijr[pos[0]][pos[1]][k], -rectangles[k].c);
+            }
+        }
+        return objective;
+    }
+
+     public static void addConstraints(
+            MPSolver solver,
+            int m, int n, int r,
+            int[][] board,
+            Rectangle[] rectangles,
+            MPVariable[][][] x_ijr,
+            MPVariable[][] y_ij,
+            Map<Integer, ArrayList<int[]>> A_r,
+            Map<Integer, Map<int[], ArrayList<int[]>>> B_ijr) {
+                for (int k = 0; k < r; k++) {
+                    MPConstraint constraint = solver.makeConstraint(0, 1, "");
+                    for (int[] pos : A_r.get(k)) {
+                        constraint.setCoefficient(x_ijr[pos[0]][pos[1]][k], 1);
+                    }
+                }
+
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (board[i][j] > 0) {
+                            MPConstraint constraint = solver.makeConstraint(-solver.infinity(), 0, "");
+                            constraint.setCoefficient(y_ij[i][j], 1);
+                            for (int k = 0; k < r; k++) {
+                                Map<int[], ArrayList<int[]>> posMap = B_ijr.get(k);
+                                ArrayList<int[]> coveringPositions = posMap.getOrDefault(new int[] {i, j}, new ArrayList<>());
+                                for (int[] pos : coveringPositions) {
+                                    constraint.setCoefficient(x_ijr[pos[0]][pos[1]][k], k);
+                                }
+                            }
+                        } else if (board[i][j] < 0) {
+                            MPConstraint constraint = solver.makeConstraint(solver.infinity(), 0, "");
+                            constraint.setCoefficient(y_ij[i][j], r);
+                            for (int k = 0; k < r; k++) {
+                                Map<int[], ArrayList<int[]>> posMap = B_ijr.get(k);
+                                ArrayList<int[]> coveringPositions = posMap.getOrDefault(new int[]{ i, j }, new ArrayList<>());
+                                for (int[] pos : coveringPositions) {
+                                    constraint.setCoefficient(x_ijr[pos[0]][pos[1]][k], -1);
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+            }
     public static void main( String[] args ) {
         Scanner sc = new Scanner(System.in);
         System.out.println( "Hello World!" );
@@ -24,8 +101,10 @@ public class App {
         int n = sc.nextInt();
         int r = sc.nextInt();
 
+        Loader.loadNativeLibraries();
+
         int[][] board = new int[m][n];
-        int[] rectangles = new int[r];
+        Rectangle[] rectangles = new Rectangle[r];
 
         Map<Integer, ArrayList<int[]>> A_r = new HashMap<>();
         Map<Integer, Map<int[], ArrayList<int[]>>> B_ijr = new HashMap<>();
@@ -36,7 +115,10 @@ public class App {
             }
         }
         for (int i = 0; i < r; i++) {
-            rectangles[i] = sc.nextInt();
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            int c = sc.nextInt();
+            rectangles[i] = new Rectangle(h, w, c);
         }
 
         MPSolver solver = MPSolver.createSolver("BoardPackaging");
@@ -45,26 +127,47 @@ public class App {
             return;
         }
 
-        int[][][] x_ijr = new int[m][n][r];
-        int[][] y_ijr = new int[m][n];
+        MPVariable[][][] x_ijr = new MPVariable[m][n][r];
+        MPVariable[][] y_ij = new MPVariable[m][n];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < r; k++) {
-                    x_ijr[i][j][k] = 0;
+                    x_ijr[i][j][k] = solver.makeIntVar(0, 1, "");
                 }
             }
         }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                y_ijr[i][j] = 0;
+                y_ij[i][j] = solver.makeIntVar(0, 1, "");
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] > 0) {}
+        MPObjective objective = addObjective(solver, m, n, r, board, rectangles, x_ijr, y_ij, A_r, B_ijr);
+
+        System.out.println("Number of variable: " + solver.numVariables());
+        System.out.println("Number of constraints: " + solver.numConstraints());
+        addConstraints(solver, m, n, r, board, rectangles, x_ijr, y_ij, A_r, B_ijr);
+        final MPSolver.ResultStatus resultStatus = solver.solve();
+        if (resultStatus == MPSolver.ResultStatus.OPTIMAL) {
+            System.out.println("Optimal solution!:");
+            System.out.println("Objective value: " + objective.value());
+            System.out.println("Solution value: ");
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(y_ij[i][j].solutionValue());
+                }
+                System.out.println();
+            }
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < r; k++) {
+                        System.out.print(x_ijr[i][j][k]);
+                    }
+                    System.out.println();
+                }
+                System.out.println();
             }
         }
     }
